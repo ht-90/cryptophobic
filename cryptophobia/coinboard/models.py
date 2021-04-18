77d061cfa9271865ec673414a9274300fdc9b2d6
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-04-18 15:20:16 +1000

    added Coin model

--- cryptophobia/coinboard/models.py
+++ cryptophobia/coinboard/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        auto_created=True,
+    )
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        verbose_name="Coin Name",
+    )
+    ticker = models.CharField(
+        max_length=20,
+        blank=False,
+        null=False,
+        verbose_name="Coin Alias",
+    )
+    address = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True,
+        verbose_name="Coin Address",
+    )
+
+    def __str__(self):
+        return self.name
