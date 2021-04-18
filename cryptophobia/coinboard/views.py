4356956a5aa80f289186ddd5f9ffcb19b12313c8
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-04-18 20:52:38 +1000

    added serializer for Coin, and view and url for api

--- cryptophobia/coinboard/views.py
+++ cryptophobia/coinboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Coin
+from .serializers import CoinSerializer
+
+
+class CoinList(generics.ListCreateAPIView):
+    queryset = Coin.objects.all()
+    serializer_class = CoinSerializer
+
 
 # Create your views here.
