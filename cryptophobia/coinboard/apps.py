4da48eedc1a9c7d41311d20305bf645582058216
Hayato.Takasaki <hayato.taka75@gmail.com>
2021-04-18 15:21:36 +1000

    added coinboard app and rest_framework

--- cryptophobia/coinboard/apps.py
+++ cryptophobia/coinboard/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CoinboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coinboard'
+    name = 'cryptophobia.coinboard'
